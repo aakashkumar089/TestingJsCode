3b88f6f02abe0c326ed29e46d6ce995a41f0cbaf
aakashkumar089 <aakashkumar089@gmail.com>
2023-02-06 08:02:45 +0530

    Object added

---
+++ Objects/Objects.js
@@ -0,0 +1,12 @@
+const person = {
+  name: "John",
+  age: 34,
+  hobbies: ["Cooking", "Sports"],
+  greet: function () {
+    alert("Hello John!");
+  },
+};
+
+person.greet();
+
+console.log("person", person);
