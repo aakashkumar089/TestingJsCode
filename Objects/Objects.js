54c019315938536c50218bc943367f787f8a886c
aakashkumar089 <aakashkumar089@gmail.com>
2023-02-06 09:10:48 +0530

    learned working with object properties and fetching data from object

--- Objects/Objects.js
+++ Objects/Objects.js
@@ -1,3 +1,8 @@
+//This below cont variable = anonymous variable line is added at last
+
+const anonymousValue =
+  "Added later in above code and now i want this into object";
+
 const person = {
   name: "John",
   age: 34,
@@ -5,8 +10,18 @@ const person = {
   greet: function () {
     alert("Hello John!");
   },
+
+  "working place": "Korba",
+  "81095452.10": "Phone number",
+  [anonymousValue]: "New Line",
 };
 
-person.greet();
+(person.age = 50), console.log((person.isAdmin = true));
 
-console.log("person", person);
+//delete person.age; //For delete the age completely
+person.age = null; // means person's age is present but value is not present
+console.log(person.age);
+console.log(person["working place"]);
+console.log(person["81095452.10"]);
+console.log(person[anonymousValue]);
+console.log(person); // At the end  the key's are automatically converted into strings
