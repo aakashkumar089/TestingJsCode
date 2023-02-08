7fba6e49f9ba72b2a95fcdc9695188f793946c3e
aakashkumar089 <aakashkumar089@gmail.com>
2023-02-08 07:37:41 +0530

    origin

---
+++ Objects/DemoApp.js
@@ -0,0 +1,25 @@
+const addMovieBtn = document.getElementById("add-movie-btn");
+const searchBtn = document.getElementById("search-btn");
+
+const movies = [];
+const addMovieHandler = () => {
+  const title = document.getElementById("title").value;
+  const extraName = document.getElementById("extra-name").value;
+  const extraValue = document.getElementById("extra-value").value;
+
+  if (title.trim() == "" || extraName.trim() == "" || extraValue.trim() == "") {
+    return alert("Cannot be empty");
+  }
+
+  const newMovie = {
+    infoKey: {
+      title,
+      [extraName]: extraValue,
+    },
+    id: Math.random(),
+  };
+  movies.push(newMovie);
+  console.log(newMovie);
+};
+
+addMovieBtn.addEventListener("click", addMovieHandler);
