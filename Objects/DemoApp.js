d9982d7669dc11c6db8b0ac1e2932d8717f11bfd
aakashkumar089 <aakashkumar089@gmail.com>
2023-02-10 08:26:02 +0530

    added in Demo App

--- Objects/DemoApp.js
+++ Objects/DemoApp.js
@@ -2,6 +2,36 @@ const addMovieBtn = document.getElementById("add-movie-btn");
 const searchBtn = document.getElementById("search-btn");
 
 const movies = [];
+
+const renderMovies = (filter = "") => {
+  const movieList = document.getElementById("movie-list");
+
+  if (movies.length === 0) {
+    movieList.classList.remove("visible");
+    return;
+  } else {
+    movieList.classList.add("visible");
+  }
+  movieList.innerHTML = "";
+
+  const filteredMovies = !filter
+    ? movies
+    : movies.filter((movie) => movie.infoKey.title.includes(filter));
+
+  filteredMovies.forEach((movie) => {
+    const movieEl = document.createElement("li");
+
+    let text = movie.infoKey.title + "-";
+
+    for (const key in movie.infoKey) {
+      if (key !== "title") {
+        text = text + `${key}: ${movie.infoKey[key]}`;
+      }
+    }
+    movieEl.textContent = text;
+    movieList.append(movieEl);
+  });
+};
 const addMovieHandler = () => {
   const title = document.getElementById("title").value;
   const extraName = document.getElementById("extra-name").value;
@@ -20,6 +50,13 @@ const addMovieHandler = () => {
   };
   movies.push(newMovie);
   console.log(newMovie);
+  renderMovies();
 };
 
+const searchMovieFilter = () => {
+  const filterTerm = document.getElementById("filter-title").value;
+  renderMovies(filterTerm);
+};
 addMovieBtn.addEventListener("click", addMovieHandler);
+
+searchBtn.addEventListener("click", searchMovieFilter);
